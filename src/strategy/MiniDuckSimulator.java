3d7ae598987acd9d4bbe1c0e53ab11776f0a794a
Ameya Shahu <ameyashahu@gmail.com>
2024-01-18 03:48:10 -0700

    Implemented Strategy pattern

---
+++ src/strategy/MiniDuckSimulator.java
@@ -0,0 +1,17 @@
+package strategy;
+
+public class MiniDuckSimulator {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.display();
+        mallard.performFly();
+        mallard.performQuack();
+
+        Duck model = new ModelDuck();
+        model.display();
+        model.performFly();
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
+        model.performQuack();
+    }
+}
